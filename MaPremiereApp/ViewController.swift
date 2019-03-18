e7fae6f8a8edb4cc606bb0603ead4497d023fb2a
BoomPowBep <37978668+BoomPowbep@users.noreply.github.com>
2019-03-18 14:39:25 +0100

    Initial Commit

---
+++ MaPremiereApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MaPremiereApp
+//
+//  Created by Mickaël Debalme on 18/03/2019.
+//  Copyright © 2019 Mickaël Debalme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MaPremiereAppTests/MaPremiereAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  MaPremiereAppTests.swift
+//  MaPremiereAppTests
+//
+//  Created by Mickaël Debalme on 18/03/2019.
+//  Copyright © 2019 Mickaël Debalme. All rights reserved.
+//
+
+import XCTest
+@testable import MaPremiereApp
+
+class MaPremiereAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
